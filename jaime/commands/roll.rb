c2aa74bc392392a197e1e80a24be548d2f262537
Dokthar <jmaselbas@gmail.com>
2016-06-29 00:01:17 +0200

    added help entry, fancier output, better regexp matching

--- jaime/commands/roll.rb
+++ jaime/commands/roll.rb
@@ -11,19 +11,20 @@ module Jaime
 
             command 'roll' do |client, data, _match|
                 if (_match["expression"] == nil) then
-                    str = "Rolling 1D100 !\n"
-                    str += rollDices(1, 100).to_s;
+                    str = "Rolling 1 100-faced dice !\n"
+                    str += Jaime::Commands::Rand::rollDices(1, 100).to_s;
                     client.say(channel: data.channel, text: str);
-                elsif (/d|D/.match(_match["expression"]) ) then
-                    dicesArgs = _match["expression"].split(/d|D/)
+                elsif (/^\d+(d|D| )\d+$/.match(_match["expression"].strip) ) then
+                    dicesArgs = _match["expression"].strip.split(/d|D| /)
                     if(dicesArgs.length == 2) then
-                        str += rollDices(dicesArgs[0].to_i,dicesArgs[1].to_i).to_s;
+                        str = "Rolling " + dicesArgs[0] + " " + dicesArgs[1]+ "-faced dice" + (dicesArgs[0].to_i > 1 ? "s" : "") + " for you !\n"
+                        str += Jaime::Commands::Rand::rollDices(dicesArgs[0].to_i,dicesArgs[1].to_i).to_s;
                         client.say(channel: data.channel, text: str);
                     else
-                        Jaime::Util::replyByWhisper(client, data, "Usage: `rand <nbDices>D<nbFaces>`");
+                        Jaime::Util::replyByWhisper(client, data, "Usage: `roll <nbDices>D<nbFaces>`",false);
                     end
                 else
-                    Jaime::Util::replyByWhisper(client, data, "Usage: `rand <nbDices>D<nbFaces>`");
+                    Jaime::Util::replyByWhisper(client, data, "Usage: `roll <nbDices>D<nbFaces>`",false);
                 end
             end
         end
